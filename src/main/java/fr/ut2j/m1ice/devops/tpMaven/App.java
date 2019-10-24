6ea1c0b5711681028235505de7bd966052039bf4
Quentin <quentin.ceschin@gmail.com>
2019-10-24 15:36:49 +0200

    init repo with starting jenkins

---
+++ src/main/java/fr/ut2j/m1ice/devops/tpMaven/App.java
@@ -0,0 +1,13 @@
+package fr.ut2j.m1ice.devops.tpMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
