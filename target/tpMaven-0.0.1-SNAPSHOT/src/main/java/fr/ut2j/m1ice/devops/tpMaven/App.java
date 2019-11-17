f94e5f794e6ab3ff718555ac017d75299804c4bc
Quentin <quentin.ceschin@gmail.com>
2019-11-17 18:55:27 +0100

    end to zip

---
+++ target/tpMaven-0.0.1-SNAPSHOT/src/main/java/fr/ut2j/m1ice/devops/tpMaven/App.java
@@ -0,0 +1,14 @@
+package fr.ut2j.m1ice.devops.tpMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Starting APP ...." );
+        JXBusyLabelTest.main(null);
+    }
+}
